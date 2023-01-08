89602ae5be73bf8ad78f00db4e535f5e8ae94a10
Pedro-HenriqueDev <ph1305803@gmail.com>
2023-01-07 21:31:08 -0300

    fix cors

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import express from 'express'
 import { AppDataSource } from './data-source'
 import { errorMiddleware } from './middlewares/error'
 import routes from './routes'
+import cors from 'cors'
 
 const port = process.env.PORT || 3000
 
 AppDataSource.initialize().then(() => {
 	const app = express()
 
+	app.use(cors())
 	app.use(express.json())
 
 	app.use(routes)
