f38557ac494a84db89b7c3d6a3400dd7a69819c0
Pedro-HenriqueDev <ph1305803@gmail.com>
2023-01-07 21:12:46 -0300

    fix vercel problem

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ const express_1 = __importDefault(require("express"));
 const data_source_1 = require("./data-source");
 const error_1 = require("./middlewares/error");
 const routes_1 = __importDefault(require("./routes"));
+const port = process.env.PORT || 3000;
 data_source_1.AppDataSource.initialize().then(() => {
     const app = (0, express_1.default)();
     app.use(express_1.default.json());
     app.use(routes_1.default);
     app.use(error_1.errorMiddleware);
-    return app.listen(process.env.PORT);
+    return app.listen(port);
 });
