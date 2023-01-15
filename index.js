062859b7b2e10ae42501f9e25e4f049b51b7a401
Pedro-HenriqueDev <ph1305803@gmail.com>
2023-01-15 11:14:26 -0300

    add build celebrate

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 require("express-async-errors");
 const express_1 = __importDefault(require("express"));
 const data_source_1 = require("./dist/data-source");
-const error_1 = require("./dist/middlewares/error");
 const routes_1 = __importDefault(require("./dist/routes"));
 const cors_1 = __importDefault(require("cors"));
+const authMiddleware_1 = require("./dist/middlewares/authMiddleware");
 const port = process.env.PORT || 3000;
 data_source_1.AppDataSource.initialize().then(() => {
     const app = (0, express_1.default)();
     app.use((0, cors_1.default)());
     app.use(express_1.default.json());
     app.use(routes_1.default);
-    app.use(error_1.errorMiddleware);
+    app.use(authMiddleware_1.celebrateErrorValidator);
     return app.listen(port);
 });
