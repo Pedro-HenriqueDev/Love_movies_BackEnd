01db2bc6b68eac46fad096f8572b100d2c3c388f
Pedro-HenriqueDev <ph1305803@gmail.com>
2023-05-03 21:03:30 -0300

    atualização do CORS

--- src/index.ts
+++ src/index.ts
@@ -10,14 +10,7 @@ const port = process.env.PORT || 3000
 AppDataSource.initialize().then(() => {
 	const app = express()
 
-	
-	const allowedOrigins = ['http://localhost:8080'];
-
-	const options: cors.CorsOptions = {
-	origin: allowedOrigins
-	};
-
-	app.use(cors(options));
+	app.use(cors());
 
 	app.use(express.json())
 
