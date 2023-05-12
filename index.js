e742432e1ee66ad41245409b67d61537ad87368b
Pedro Henrique <82292364+Pedro-HenriqueDev@users.noreply.github.com>
2023-05-12 10:55:20 -0300

    Update index.js cors

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 3000;
 data_source_1.AppDataSource.initialize().then(() => {
     const app = (0, express_1.default)();
     const options = {
-        origin: true,
+        origin: false,
         methods: ["GET", "OPTIONS", "PUT", "POST", "DELETE"],
     };
     app.use((0, cors_1.default)(options));
