928d601ed867ac2fb0ab2ed0aac38ec73636cc12
Pedro-HenriqueDev <ph1305803@gmail.com>
2023-05-11 22:29:35 -0300

    fix error Cors

--- src/index.ts
+++ src/index.ts
@@ -9,14 +9,6 @@ const port = process.env.PORT || 3000
 
 AppDataSource.initialize().then(() => {
 	const app = express()
-
-	app.use((req: Request, res: Response, next: NextFunction) => {
-		res.setHeader("Access-Control-Allow-Credentials" , "true")
-		res.setHeader("Access-Control-Allow-Origin" , "https://love-movie.vercel.app")
-		res.setHeader("Access-Control-Allow-Methods" , "GET,OPTIONS,PATCH,DELETE,POST,PUT")
-		res.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization")
-		next()
-	})
 	app.use(cors());
 	app.use(express.json())
 
