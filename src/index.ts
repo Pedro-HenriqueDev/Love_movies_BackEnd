375818444f88d8818fe2602ba3850c08fa39c6c7
Pedro-HenriqueDev <ph1305803@gmail.com>
2023-05-11 23:07:52 -0300

    fix error Cors

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import routes from './routes'
 import cors from 'cors'
 import { celebrateErrorValidator } from './middlewares/authMiddleware'
 
-const allowedOrigins = ['https://love-movie.vercel.app/#/'];
+const allowedOrigins = ['https://love-movie.vercel.app'];
 
 const port = process.env.PORT || 3000
 
